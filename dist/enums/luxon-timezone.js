d46598f3caa43db12d0a7f71b451aeced31c8e3f
it@plabelclothing.com <it@plabelclothing.com>
2021-11-16 22:03:52 +0100

    deleted dist

--- dist/enums/luxon-timezone.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LuxonTimezone = void 0;
-var LuxonTimezone;
-(function (LuxonTimezone) {
-    LuxonTimezone["X"] = "X";
-})(LuxonTimezone = exports.LuxonTimezone || (exports.LuxonTimezone = {}));
--- dist/enums/validation-const.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidationConst = void 0;
-exports.ValidationConst = {
-    coupleType: ['BTC_USD', 'BTC_EUR'],
-    crypto: ['BTC'],
-};
